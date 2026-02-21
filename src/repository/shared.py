6ea3a413b4017606627419571a71a40ab060f8f2
Khunthynne <khunthynne@gmail.com>
2026-02-21 08:32:20 +0700

    refactor: update prisma repo (#44)

---
+++ src/repository/shared.py
@@ -0,0 +1,25 @@
+from typing import Any, Generic, TypeVar
+
+from core.db import prisma
+
+T = TypeVar("T")
+
+
+class BaseRepository(Generic[T]):
+    def __init__(self, model_name: str) -> None:
+        self.model = getattr(prisma, model_name)
+
+    async def get_all(self) -> list[T]:
+        return await self.model.find_many()
+
+    async def get_by_id(self, id: str) -> T | None:
+        return await self.model.find_unique(where={"id": id})
+
+    async def create(self, data: Any) -> T:
+        return await self.model.create(data=data)
+
+    async def update(self, id: str, data: Any) -> T | None:
+        return await self.model.update(where={"id": id}, data=data)
+
+    async def delete(self, id: str) -> T | None:
+        return await self.model.delete(where={"id": id})
